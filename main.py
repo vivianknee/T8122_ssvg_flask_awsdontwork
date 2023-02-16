786a116d0b522559c8f3b6ea5ad7c410eaa0c9b2
Vivian <vivianni452@gmail.com>
2023-02-16 14:17:59 -0800

    cleaning up system

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ from flask import render_template  # import render_template from "public" flask 
 
 # import "packages" from "this" project
 from __init__ import app  # Definitions initialization
-from model.jokes import initJokes
 from model.users import initUsers
 from model.cars import initCars
 from model.carspecs import initCarSpec
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.car import car_api
 from api.carspec import carspec_api
@@ -21,7 +19,6 @@ from api.carspec import carspec_api
 from projects.projects import app_projects # Blueprint directory import projects definition
 
 # register URIs
-app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(car_api) # register api routes
@@ -43,7 +40,6 @@ def stub():
 
 @app.before_first_request
 def activate_job():
-    #initJokes()
     initUsers()
     initCars()
     initCarSpec()
