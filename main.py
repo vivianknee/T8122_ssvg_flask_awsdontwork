e43e2896fcaa2f2f85bc04627b6fce9db26a12f4
Vivian <vivianni452@gmail.com>
2023-02-17 11:42:55 -0800

    i have an extenstion

--- main.py
+++ main.py
@@ -47,6 +47,6 @@ def activate_job():
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing
-    from flask_cors import CORS
-    cors = CORS(app)
+    #from flask_cors import CORS
+    #cors = CORS(app)
     app.run(debug=True, host="0.0.0.0", port="8080")
